364a5ee139cfed8d7966177d93fe22cf3f50663c
AAxxel <axelleonardo086@gmail.com>
2025-02-03 11:43:52 -0600

    Se establecio conexion con la base de datos

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./src/config/app');
 const server = require('http').createServer(app);
+const sequelize = require('./src/config/database');
 require('dotenv').config();
 
 const port = process.env.PORT;
@@ -8,9 +9,21 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-server.listen(port, () => { 
-    console.log(`Server corriendo en la ruta http://localhost:${port}`);
-});
 
+
+  const startServer = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+        server.listen(port, () => { 
+            console.log(`Server corriendo en la ruta http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+  }
+
+
+startServer();
 
 
