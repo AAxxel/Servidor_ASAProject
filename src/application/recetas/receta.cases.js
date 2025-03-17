808be8fa718ac1b192afb5fb8c061305eb7b21ef
AAxxel <axelleonardo086@gmail.com>
2025-03-16 18:26:10 -0600

    sigue fastidiado

--- src/application/recetas/receta.cases.js
+++ src/application/recetas/receta.cases.js
@@ -1,37 +1,32 @@
 const recetaDtos = require('./receta.dtos.js');
 const { RecetaRepository } = require('../../domain/repositories/repositories.js');
+const { getEspecieById } = require('../../domain/services/especies.services.js');
 
 const getAllCase = async () => { 
     const list = await RecetaRepository.obtenerRecetaConTratamiento();
-    const response = list.map((item) => new recetaDtos.RecetaResponse(item));
-    return response;
-}
-
-const getAllCaseTratamiento = async (id) => { 
-    const list = await RecetaRepository.getPerTratamiento(id);
-    const response = list.map((item) => new recetaDtos.dtoResponseList(item));
+    const response = list.map((item) => new recetaDtos.dtoResponse(item));
     return response;
 }
 
 const getCase = async (id) => { 
     const getObject = await RecetaRepository.getById(id);
-    return new recetaDtos.RecetaResponseOnly(getObject);
+    return new recetaDtos.dtoResponseOnly(getObject);
 };
 
 const createCase = async (data) => {
     const object = new recetaDtos.dtoCreate(data);
     const createdObject = await RecetaRepository.create(object);
-    return createdObject;
+    return new recetaDtos.dtoResponse(createdObject);
 }
 
 const updateCase = async (data) => {
     const object = new recetaDtos.dtoUpdate(data);
     const updatedObject = await RecetaRepository.update(object);
-    return updatedObject;
+    return new recetaDtos.dtoResponse(updatedObject);
 }
 
 const destroyCase = async (id) => {
     return await RecetaRepository.destroy(id);
 }
 
-module.exports = { getAllCase, getCase, getAllCaseTratamiento, createCase, updateCase, destroyCase };
+module.exports = { getAllCase, getCase, createCase, updateCase, destroyCase };
